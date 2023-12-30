4f8b48d03102e4766f1925ebde10af045c053493
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2023-12-30 18:18:26 +0100

    typo & documentation

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject de.dixieflatline/flimfam "0.1.0-SNAPSHOT"
+(defproject de.dixieflatline/flimflam "0.1.0-SNAPSHOT"
   :description "Email address validation."
-  :url "https://github.com/20centaurifux/flimfam"
+  :url "https://github.com/20centaurifux/flimflam"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]

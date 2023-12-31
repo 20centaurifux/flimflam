4459606f3513b0dc72700d521ffeb5b2b033fb57
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2023-12-31 10:32:43 +0100

    moved generators to test directory

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [instaparse "1.4.12"]
-                 [org.clojure/test.check "1.1.1"]
-                 [com.velisco/strgen "0.2.5"]]
+                 [instaparse "1.4.12"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
+  :profiles {:uberjar {:aot :all}
+             :test {:dependencies [[org.clojure/test.check "1.1.1"]
+                                   [com.velisco/strgen "0.2.5"]]}}
   :plugins [[lein-marginalia "0.9.1"]
             [dev.weavejester/lein-cljfmt "0.12.0"]]
   :cljfmt {:load-config-file? true})
